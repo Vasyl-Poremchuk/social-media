509f09886ff301bd1b0479b18f29a75af9a91f21
Vasyl Poremchuk <nleveryone@gmail.com>
2023-04-18 11:42:51 +0300

    Add a database.py file with a get_db function

---
+++ src/database.py
@@ -0,0 +1,31 @@
+from typing import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://"
+    f"{settings.POSTGRES_USER}:"
+    f"{settings.POSTGRES_PASSWORD}@"
+    f"{settings.POSTGRES_HOST}:"
+    f"{settings.POSTGRES_PORT}/"
+    f"{settings.POSTGRES_DB}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    """
+    The function of creating and closing the session database.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
