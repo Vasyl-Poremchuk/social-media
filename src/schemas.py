afe133292ba3043d686d73aa60672219bc7c998c
Vasyl Poremchuk <nleveryone@gmail.com>
2023-04-18 11:50:13 +0300

    Add a schemas.py file with the required model schemas

---
+++ src/schemas.py
@@ -0,0 +1,391 @@
+import re
+from datetime import datetime
+from string import punctuation
+
+from pydantic import BaseModel, EmailStr, validator
+
+from src.exceptions import (
+    UsernameFormatError,
+    PasswordFormatError,
+    FirstNameFormatError,
+    LastNameFormatError,
+    PhoneNumberFormatError,
+    CountryFormatError,
+    TitleFormatError,
+    ContentFormatError,
+)
+from src.models import Category
+
+
+PHONE_NUMBER_REGEX = re.compile(
+    r"^(?:\+38|0)?\(?0?\d{2}\)?\s?\d{3}-?\d{2}-?\d{2}$"
+)
+PUNCTUATION_WITHOUT_UNDERSCORE = punctuation.replace("_", "")
+
+
+def validate_content(content: str) -> str:
+    """
+    The function checks if the `content` passes all validations.
+    """
+    if all(
+        character.isdigit() or character.isspace() or character in punctuation
+        for character in content
+    ):
+        raise ContentFormatError(
+            content=content,
+            message="The `content` must contain letters.",
+        )
+    return content
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    first_name: str | None = None
+    last_name: str | None = None
+    phone_number: str | None = None
+    country: str | None = None
+    region: str | None = None
+
+    @validator("username")
+    def validate_username(cls, value: str) -> str:
+        """
+        The method checks if the `username` passes all validations.
+        """
+        if not (5 <= len(value) <= 15):
+            raise UsernameFormatError(
+                username=value,
+                message="The length of the `username` must be between 5 and 15 characters.",
+            )
+
+        if any(character.isupper() for character in value):
+            raise UsernameFormatError(
+                username=value,
+                message="The `username` must not contain uppercase characters.",
+            )
+
+        if any(character.isspace() for character in value):
+            raise UsernameFormatError(
+                username=value,
+                message="The `username` must not contain whitespaces.",
+            )
+
+        if any(
+            character in PUNCTUATION_WITHOUT_UNDERSCORE for character in value
+        ):
+            raise UsernameFormatError(
+                username=value,
+                message="The `username` must not contain any punctuation marks other than underscores.",
+            )
+
+        return value
+
+    @validator("password")
+    def validate_password(cls, value: str) -> str:
+        """
+        The function checks if the `password` passes all validations.
+        """
+        if len(value) < 8:
+            raise PasswordFormatError(
+                password=value,
+                message="The `password` must be at least 8 characters long.",
+            )
+
+        if not any(character.isupper() for character in value):
+            raise PasswordFormatError(
+                password=value,
+                message="The `password` must contain at least one uppercase letter.",
+            )
+
+        if not any(character.islower() for character in value):
+            raise PasswordFormatError(
+                password=value,
+                message="The `password` must contain at least one lowercase letter.",
+            )
+
+        if not any(character.isdigit() for character in value):
+            raise PasswordFormatError(
+                password=value,
+                message="The `password` must contain at least one digit.",
+            )
+
+        if not any(character in punctuation for character in value):
+            raise PasswordFormatError(
+                password=value,
+                message="The `password` must contain at least one punctuation mark "
+                "such as !#$%&'\"()*+,-/:;<=>?@[\\]^_`{|}~.",
+            )
+
+        if any(character.isspace() for character in value):
+            raise PasswordFormatError(
+                password=value,
+                message="The `password` must not contain whitespaces.",
+            )
+
+        return value
+
+    @validator("first_name")
+    def validate_first_name(cls, value: str) -> str:
+        """
+        The method checks if the `first_name` passes all validations.
+        """
+        if value:
+            if any(character.isspace() for character in value):
+                raise FirstNameFormatError(
+                    first_name=value,
+                    message="The `first_name` must not contain whitespaces.",
+                )
+
+            if not value.istitle():
+                raise FirstNameFormatError(
+                    first_name=value,
+                    message="The `first_name` must start with a capital letter.",
+                )
+
+            if any(character.isdigit() for character in value):
+                raise FirstNameFormatError(
+                    first_name=value,
+                    message="The `first_name` must not contain digits.",
+                )
+
+            if any(character in punctuation for character in value):
+                raise FirstNameFormatError(
+                    first_name=value,
+                    message="The `first_name` must not contain punctuation marks.",
+                )
+
+        return value
+
+    @validator("last_name")
+    def validate_last_name(cls, value: str) -> str:
+        """
+        The method checks if the `last_name` passes all validations.
+        """
+        if value:
+            if any(character.isspace() for character in value):
+                raise LastNameFormatError(
+                    last_name=value,
+                    message="The `last_name` must not contain whitespaces.",
+                )
+
+            if not value.istitle():
+                raise LastNameFormatError(
+                    last_name=value,
+                    message="The `last_name` must start with a capital letter.",
+                )
+
+            if any(character.isdigit() for character in value):
+                raise LastNameFormatError(
+                    last_name=value,
+                    message="The `last_name` must not contain digits.",
+                )
+
+            if any(character in punctuation for character in value):
+                raise LastNameFormatError(
+                    last_name=value,
+                    message="The `last_name` must not contain punctuation marks.",
+                )
+
+        return value
+
+    @validator("phone_number")
+    def validate_phone_number(cls, value: str) -> str:
+        """
+        The method checks if the `phone_number` passes all validations.
+        """
+        if value:
+            if not PHONE_NUMBER_REGEX.match(value):
+                raise PhoneNumberFormatError(
+                    phone_number=value,
+                    message="The `phone_number` is in the wrong format.",
+                )
+
+        return value
+
+    @validator("country")
+    def validate_country(cls, value: str) -> str:
+        """
+        The method checks if the `country` passes all validations.
+        """
+        if value:
+            if any(character in punctuation for character in value):
+                raise CountryFormatError(
+                    country=value,
+                    message="The `country` must not contain punctuation marks.",
+                )
+
+            if any(character.isdigit() for character in value):
+                raise CountryFormatError(
+                    country=value,
+                    message="The `country` must not contain digits.",
+                )
+
+            if not value.istitle():
+                raise CountryFormatError(
+                    country=value,
+                    message="The `country` must start with a capital letter.",
+                )
+
+        return value
+
+    @validator("region")
+    def validate_region(cls, value: str) -> str:
+        """
+        The method checks if the `region` passes all validations.
+        """
+        if value:
+            if any(character in punctuation for character in value):
+                raise CountryFormatError(
+                    country=value,
+                    message="The `region` must not contain punctuation marks.",
+                )
+
+            if any(character.isdigit() for character in value):
+                raise CountryFormatError(
+                    country=value,
+                    message="The `region` must not contain digits.",
+                )
+
+            if not value.istitle():
+                raise CountryFormatError(
+                    country=value,
+                    message="The `region` must start with a capital letter.",
+                )
+
+        return value
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    user_id: int
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserSummaryResponse(BaseModel):
+    user_id: int
+    username: str
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenPayload(BaseModel):
+    user_id: int | None = None
+
+
+class CommentBase(BaseModel):
+    content: str
+    post_id: int
+
+    _validate_content = validator("content", allow_reuse=True)(
+        validate_content
+    )
+
+
+class CommentCreate(CommentBase):
+    pass
+
+
+class CommentUpdate(CommentBase):
+    pass
+
+
+class CommentResponse(CommentBase):
+    comment_id: int
+    created_at: datetime
+    updated_at: datetime
+    author: UserSummaryResponse
+
+    class Config:
+        orm_mode = True
+
+
+class LikeBase(BaseModel):
+    post_id: int
+    liked: bool
+
+
+class LikeResponse(LikeBase):
+    like_id: int
+    created_at: datetime
+    user: UserSummaryResponse
+
+    class Config:
+        orm_mode = True
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    category: Category
+
+    @validator("title")
+    def validate_title(cls, value: str) -> str:
+        """
+        The method checks if the `title` passes all validations.
+        """
+        if all(
+            character.isdigit()
+            or character.isspace()
+            or character in punctuation
+            for character in value
+        ):
+            raise TitleFormatError(
+                title=value,
+                message="The `title` must contain letters.",
+            )
+
+        return value
+
+    _validate_content = validator("content", allow_reuse=True)(
+        validate_content
+    )
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostUpdate(PostBase):
+    pass
+
+
+class PostResponse(PostBase):
+    post_id: int
+    created_at: datetime
+    updated_at: datetime
+    author: UserSummaryResponse
+
+    class Config:
+        orm_mode = True
+
+
+class PostLikeCommentResponse(BaseModel):
+    Post: PostResponse
+    likes: int
+    comments: int
+
+    class Config:
+        orm_mode = True
