0214fde24d7b4eb9a9cde81836bde0c61a2c8e23
Vasyl Poremchuk <nleveryone@gmail.com>
2023-06-19 16:57:11 +0300

    Minor changes

--- alembic/env.py
+++ alembic/env.py
@@ -8,8 +8,6 @@ from alembic import context
 from src.models import Base
 from src.database import SQLALCHEMY_DATABASE_URL
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
 section = config.config_ini_section
@@ -18,19 +16,10 @@ config.set_section_option(
     section, "SQLALCHEMY_DATABASE_URL", SQLALCHEMY_DATABASE_URL
 )
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
